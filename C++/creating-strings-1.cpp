927054b39419e6b42a4e16fabb03586b118230f0
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-09-08 19:25:46 +0530

    Creating strings initial commit

---
+++ C++/creating-strings-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	string input;
+	cin >> input;
+	
+	for (char d : input)
+		cout << d - 'a' << endl;
+
+
+
+	return 0;
+}
---
+++ Python/Creating-Strings-1.py
@@ -0,0 +1,9 @@
+def create_string(seq):
+    n = len(seq)
+    print()
+
+
+if __name__ == '__main__':
+    sequence = input()
+
+    create_string(sequence)
