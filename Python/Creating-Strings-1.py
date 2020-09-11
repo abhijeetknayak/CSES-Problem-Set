2004e080759e0accec61fcc83bf566b644e72d0d
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-09-11 18:54:14 +0530

    Creating strings with permutations from itertools

--- Python/Creating-Strings-1.py
+++ Python/Creating-Strings-1.py
@@ -1,6 +1,12 @@
+from itertools import permutations
+
 def create_string(seq):
     n = len(seq)
-    print()
+    p = set(map("".join, permutations(seq, n)))  # Length of string will be used
+
+    print(len(p))
+    for strings in sorted(p):
+        print(strings)
 
 
 if __name__ == '__main__':
